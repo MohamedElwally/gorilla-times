2e164cc9932d6b7608a8fbe9fed2d0857ef7cb73
Mohamed Elwaly <mohamedelwally5@gmail.com>
2024-07-11 04:45:57 +0300

    change

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
-  return (
-    <footer>
-        &copy; All Rights Reserved By CodeWithZeeshu
-    </footer>
-  )
-}
+  return <footer>&copy; All Rights Reserved By Mohamed Abd Elwaly</footer>;
+};
 
-export default Footer
+export default Footer;
